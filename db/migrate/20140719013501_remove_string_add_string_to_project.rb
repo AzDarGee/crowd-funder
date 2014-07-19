6a10898476da760c5fc5accd538871cf12892fb8
Ash Darji <ashishdarji88@gmail.com>
2014-07-18 23:59:22 -0400

    updated

---
+++ db/migrate/20140719013501_remove_string_add_string_to_project.rb
@@ -0,0 +1,12 @@
+class RemoveStringAddStringToProject < ActiveRecord::Migration
+  def change
+    def up
+    change_column :projects, :description, :text
+    end
+    def down
+        # This might cause trouble if you have strings longer
+        # than 255 characters.
+        change_column :projects, :description, :string
+    end
+  end
+end
